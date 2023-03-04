e432ddd8a0508b016d3f507630bf4a0bad1dccee
Alex_a <shiotoy@icloud.com>
2023-03-04 20:43:32 +0300

    givven first array

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача: Написать программу, которая из имеющегося массива строк 
+// формирует массив из строк, длинна которых меньше либо равна 3 символа. 
+// Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+
+
+string[] stringArray = { "hello", "2", "world", ":)"};
+//string[] stringArray = { "1234", "1567", "-2", "computer science" };
+//string[] stringArray = { "Russia", "Denmark", "Kazan"};
+Console.WriteLine(String.Join(" ", stringArray));
